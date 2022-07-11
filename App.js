ad0744eb7fbd718140f77e97fa3344d7e41c1f81
Dmitry-nm22 <naumenkodima209@gmail.com>
2022-07-11 19:35:09 +0300

    2

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./components/Loading";
+import * as Location from 'expo-location';
+import React from "react";
+import {Alert} from "react-native";
 
 export default function App() {
+
+  // const [location, setLocation] = React.useState(null);
+  // const [errorMsg, setErrorMsg] = React.useState(null);
+  // const getLocations = async ()=> {
+  //   try {
+  //     const response = await Location.requestForegroundPermissionsAsync()
+  //     console.log(response)
+  //     const location = await Location.getCurrentPositionAsync()
+  //     console.log(location)
+  //   }catch (err){
+  //     Alert.alert('не могу определить местоположение', "сори")
+  //   }
+  // }
+  // React.useEffect(() =>{
+  //   getLocations()
+  // })/
+  // return (
+  //    <Loading />
+  // );
+
+  const [location, setLocation] = React.useState(null);
+  const [errorMsg, setErrorMsg] = React.useState(null);
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was deniedss');
+        setIsLoading(false)
+        return;
+      };
+
+      let location = await Location.getCurrentPositionAsync({});
+      setIsLoading(true)
+      setLocation(location.coords);
+    })();
+    console.log(location)
+  }, []);
+
+  // let text = 'Waiting..';
+  // if (errorMsg) {
+  //   text = errorMsg;
+  // } else if (location) {
+  //   text = JSON.stringify(location);
+  // }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      isLoading ? <Loading /> : null
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
